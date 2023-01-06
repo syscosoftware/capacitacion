5509ab8af90ed111714d62dcbf2bd893f376b455
msalamanca <jupmasalamanca@gmail.com>
2023-01-06 11:20:27 -0600

    varios

--- src/main/java/cr/sysco/controller/UsuarioController.java
+++ src/main/java/cr/sysco/controller/UsuarioController.java
@@ -1,12 +1,15 @@
 package cr.sysco.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +36,51 @@ public class UsuarioController {
         return new ResponseEntity<Map<String, Object>>(respuesta, new HttpHeaders(), HttpStatus.OK);
     }
  
+    @GetMapping("/usuario/buscar/{id}")
+    public ResponseEntity<Map<String, Object>> buscar(@PathVariable(value = "id") Integer id ) {
+
+        Map<String, Object> respuesta = new HashMap<String, Object>();   
+        respuesta.put("result",usuarioService.buscar(id)); 
+        respuesta.put("estado", HttpStatus.OK);
+
+        return new ResponseEntity<Map<String, Object>>(respuesta, new HttpHeaders(), HttpStatus.OK);
+    }
+
+
+    @PostMapping("/usuario/buscarSQL")
+    public ResponseEntity<Map<String, Object>> buscarPostSQL(@RequestBody Usuario user) {
+
+        Map<String, Object> respuesta = new HashMap<String, Object>();   
+        respuesta.put("result",usuarioService.buscarSQL(user)); 
+        respuesta.put("estado", HttpStatus.OK);
+
+        return new ResponseEntity<Map<String, Object>>(respuesta, new HttpHeaders(), HttpStatus.OK);
+    }
+
+
+    @PostMapping("/usuario/buscarHQL")
+    public ResponseEntity<Map<String, Object>> buscarPostHQL(@RequestBody Usuario user) {
+
+        Map<String, Object> respuesta = new HashMap<String, Object>();   
+        respuesta.put("result",usuarioService.buscarHQL(user)); 
+        respuesta.put("estado", HttpStatus.OK);
+
+        return new ResponseEntity<Map<String, Object>>(respuesta, new HttpHeaders(), HttpStatus.OK);
+    }
+    
+
+    @GetMapping("/usuario/listar")
+    public ResponseEntity<Map<String, Object>> listar() {
+
+        Map<String, Object> respuesta = new HashMap<String, Object>(); 
+         
+        //List<Usuario> lista = usuarioService.listar();
+        //respuesta.put("result",lista ); 
+
+         respuesta.put("result", usuarioService.listar()); 
+        respuesta.put("estado", HttpStatus.OK);
+
+        return new ResponseEntity<Map<String, Object>>(respuesta, new HttpHeaders(), HttpStatus.OK);
+    }
     
 }
